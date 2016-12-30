7b19ac5a796f051ea24a5ea3c9965f22a1c872d2
koczurekk <koczurekk@gmail.com>
2016-12-30 21:10:27 +0100

    sfcb::Callback::set function, default callback lambda

--- SFCB/Callback.hpp
+++ SFCB/Callback.hpp
@@ -11,14 +11,21 @@ namespace sfcb {
 
 	public:
 		template<typename func_t, typename ... args_t>
-		Callback(func_t func, const args_t&... args) {
+		void set(func_t func, const args_t&... args) {
 			m_func = [func, args ...](callback_args_t ... callback_args) {
 				func(callback_args ..., args ...);
 			};
 		}
 
+		template<typename func_t, typename ... args_t>
+		Callback(func_t func, const args_t&... args) {
+			this->set(func, args ...);
+		}
+
 		Callback()
-		{ }
+		{
+			m_func = [](callback_args_t ...) { };
+		}
 
 		inline void operator()(const callback_args_t ... callback_args) {
 			this->m_func(callback_args ...);
