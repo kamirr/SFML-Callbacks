63215d2bfcba290738bdc042bfa3523328a1cf2f
koczurekk <koczurekk@gmail.com>
2016-12-31 17:45:36 +0100

    Changed example TcpListener code

--- main.cpp
+++ main.cpp
@@ -1,67 +1,68 @@
 // main.cpp
 
 #include "SFCB/TcpListener.hpp"
-#include "SFCB/Window.hpp"
-
-#include <SFML/Graphics/RenderWindow.hpp>
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
 
 /* Namespace with callbacks */
 namespace callbacks {
-	void onClose(sf::RenderWindow& window, sf::Event) {
-		std::cout << "Y u do dis? ;-;" << std::endl;
-		window.close();
-	}
-
-	void onDataReceived(sfcb::TcpEvent ev, std::string& log) {
-		for(const sf::Int8 c: *ev.buffer)
-			log += c;
-
-		std::cout << "Log updated:\n" << log << std::endl;
-	}
-
 	void onAccepted(sfcb::sharedTcpSocket socket, std::vector<sfcb::sharedTcpSocket>& clients, std::string& log) {
 		std::cout << "connected (" << (*socket).getRemoteAddress() << ")" << std::endl;
 
+		static const std::string charset("abcdefghiklmonpqrstuwxyzABCDEFGHIKLMNOPQRSTUWXYZ0123456789");
+		std::string login;
+
+		int r = rand() % 255, g = rand() % 255;
+		auto background = "\033[48;2;" + std::to_string(r)     + ";" + std::to_string(g)     + ";" + std::to_string(255) + "m";
+		auto foreground = "\033[38;2;" + std::to_string(255-r) + ";" + std::to_string(255-g) + ";" + std::to_string(0)   + "m";
+		login = background + foreground;
+
+		for(int i = 0; i < 4; ++i) {
+			login += charset[size_t(rand()) % charset.size()];
+		}
+
+		login += ":\033[0m ";
+
 		static char arr[] = "Hi!\n";
 		sfcb::buffer_t buffer(std::begin(arr), std::end(arr));
-		(*socket).send(buffer);
 
-		(*socket).setCallback(sfcb::TcpEvent::DataReceived, onDataReceived, std::ref(log));
+		(*socket).send(buffer);
+		(*socket).setCallback(sfcb::SocketEvent::DataReceived, [&log, login](sfcb::SocketEvent ev) {
+			log += login;
+			for(const sf::Int8 c: *ev.buffer) {
+				log += char(c);
+			}
+		});
+
 		clients.push_back(socket);
 	}
 }
 
 int main()
 {
-	/* Create window, acts just like window given in parameter */
-	sfcb::Window<sf::RenderWindow> app({300, 300}, "app");
-	app.setFramerateLimit(60);
-
-	app.setCallback(sf::Event::Closed, app.getUniversalCallbackContext(), callbacks::onClose);
-
 	std::vector<sfcb::sharedTcpSocket> clients;
+
 	std::string log;
+	size_t oldSize = log.size();
 
 	sfcb::TcpListener listener;
 	listener.setCallback(callbacks::onAccepted, std::ref(clients), std::ref(log));
 	listener.listen(3254);
 
-	/* Minimal main loop */
-	app.clear({20, 20, 20});
-	while(app.isOpen()) {
-		/* Handle window callbacks */
-		app.handleCallbacks();
-
+	while(true) {
 		/* Handle listener callbacks */
 		listener.handleCallbacks();
 
 		/* Handle TCP callbacks */
 		sfcb::TcpSocket::handleCallbacks();
 
-		/* All callbacks are called right before displaying window */
-		app.display();
+		if(oldSize != log.size()) {
+			system("clear");
+			std::cout << log << std::flush;
+
+			oldSize = log.size();
+		}
 	}
 
 	return 0;
