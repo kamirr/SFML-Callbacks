fe62aaf266622dbec81fb4410fa6f7a79ddc6cc2
koczurekk <koczurekk@gmail.com>
2017-01-04 10:34:52 +0100

    Moved ConcurrentQueue & ResourceLoader to separate files

---
+++ SFCB/ConcurrentQueue.hpp
@@ -0,0 +1,61 @@
+#ifndef CONCURRENTQUEUE_HPP
+#define CONCURRENTQUEUE_HPP
+
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+namespace sfcb {
+	template <typename T>
+	class ConcurrentQueue {
+	public:
+		T pop() {
+			std::unique_lock<std::mutex> mlock(mutex_);
+
+			while (queue_.empty()) {
+				cond_.wait(mlock);
+			}
+
+			auto item = queue_.front();
+			queue_.pop();
+			return item;
+		}
+
+		void pop(T& item) {
+			std::unique_lock<std::mutex> mlock(mutex_);
+
+			while (queue_.empty()) {
+				cond_.wait(mlock);
+			}
+
+			item = queue_.front();
+			queue_.pop();
+		}
+
+		void push(const T& item) {
+			std::unique_lock<std::mutex> mlock(mutex_);
+			queue_.push(item);
+			mlock.unlock();
+			cond_.notify_one();
+		}
+
+		void push(T&& item) {
+			std::unique_lock<std::mutex> mlock(mutex_);
+			queue_.push(std::move(item));
+			mlock.unlock();
+			cond_.notify_one();
+		}
+
+		size_t size() {
+			return  queue_.size();
+		}
+
+	private:
+		std::mutex mutex_;
+		std::queue<T> queue_;
+		std::condition_variable cond_;
+	};
+}
+
+#endif // CONCURRENTQUEUE_HPP
---
+++ SFCB/ResourceLoader.hpp
@@ -0,0 +1,82 @@
+#ifndef RESOURCELOADER_HPP
+#define RESOURCELOADER_HPP
+
+#include <SFML/System/MemoryInputStream.hpp>
+#include <iterator>
+#include <fstream>
+
+#include "SFCB/ConcurrentQueue.hpp"
+#include "SFCB/NetworkBase.hpp"
+#include "SFCB/Callback.hpp"
+
+namespace sfcb {
+	class ResourceLoader
+		: public sf::NonCopyable {
+	private:
+		struct Request {
+			std::string path;
+			Callback<const buffer_t&> callback;
+		};
+
+		ConcurrentQueue<Request> m_requests;
+		std::vector<std::thread> m_threads;
+		bool m_asyncMode = true;
+
+		static void requestBuffer_impl(const std::string path, Callback<const buffer_t&> callback) {
+			std::ifstream is(path);
+			is >> std::noskipws;
+
+			std::istream_iterator<char> start(is), end;
+			buffer_t buffer(start, end);
+
+			callback(buffer);
+		}
+
+		static void worker(ResourceLoader* ptr) {
+			while (true) {
+				const auto req =  ptr->m_requests.pop();
+				requestBuffer_impl(req.path, req.callback);
+			}
+		}
+
+		ResourceLoader() {
+			for(auto i = 0u; i < std::thread::hardware_concurrency() - 3; ++i) {
+				this->m_threads.emplace_back(worker, this);
+			}
+		}
+
+	public:
+		static ResourceLoader& getInstance() {
+			static ResourceLoader res;
+			return res;
+		}
+
+		void requestBuffer(const std::string& path, Callback<const buffer_t&> callback) {
+			if(this->m_asyncMode) {
+				this->m_requests.push({path, callback});
+			} else {
+				this->requestBuffer_impl(path, callback);
+			}
+		}
+
+		void requestStream(const std::string& path, Callback<sf::MemoryInputStream&> callback) {
+			this->requestBuffer(path, [callback](const buffer_t& buffer) {
+				sf::MemoryInputStream stream;
+				stream.open(buffer.data(), buffer.size());
+
+				callback(stream);
+			});
+		}
+
+		void completeRemainingTasks() {
+			while(this->m_requests.size())
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+
+		void enableAsync(bool async) {
+			this->m_asyncMode = async;
+		}
+	};
+}
+
+#endif // RESOURCELOADER_HPP
