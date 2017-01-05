f13f88781567c096d15550047e7b4ebb6f784910
koczurekk <koczurekk@gmail.com>
2017-01-05 13:28:30 +0100

    Added sfcb::wrapMemberFunction

--- SFCB/Callback.hpp
+++ SFCB/Callback.hpp
@@ -30,6 +30,13 @@ namespace sfcb {
 			this->m_func(callback_args ...);
 		}
 	};
+
+	template <class ... Ts, typename func_t, class Class>
+	Callback<Ts ...> wrapMemberFunction(func_t func, Class& obj) {
+		return [func, &obj](Ts ... args) {
+			std::mem_fn(func)(obj, args ...);
+		};
+	};
 }
 
 #endif // CALLBACK_HPP
