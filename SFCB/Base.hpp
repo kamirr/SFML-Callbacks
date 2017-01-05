cd25173c9f1ef92b8cdae60de1b343f58872e9f4
koczurekk <koczurekk@gmail.com>
2017-01-05 13:27:57 +0100

    Moved some basic features not related to network to Base.hpp file

---
+++ SFCB/Base.hpp
@@ -0,0 +1,12 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <SFML/Config.hpp>
+#include <vector>
+
+namespace sfcb {
+	typedef std::vector<sf::Int8> buffer_t;
+	typedef const buffer_t cbuffer_t;
+}
+
+#endif // BASE_HPP
